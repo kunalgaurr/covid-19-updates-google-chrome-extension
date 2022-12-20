4462b7fff7d49a7adbea9c0b184b0b777df0e8cb
kunalgaurr <kunalgaur84@gmail.com>
2022-12-20 14:09:44 +0530

    Final Draft

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-fetch('https://covid-api.mmediagroup.fr/v1/cases')
-  .then((data) => data.json)
-  .then();
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e0f2b7102amshaa1596575ea8f03p17ea7fjsnf3f5f9109382',
+    'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+  },
+};
+
+const getData = (country) => {
+  countryName.innerHTML = country;
+
+  fetch(
+    'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=' +
+      country,
+    options
+  )
+    .then((response) => response.json())
+
+    .then((response) => {
+      console.log(response);
+
+      console.log(response.data);
+
+      recovered.innerHTML = response.data.recovered;
+      deaths.innerHTML = response.data.deaths;
+      confirmed.innerHTML = response.data.confirmed;
+      lastChecked.innerHTML = response.data.lastChecked;
+      lastReported.innerHTML = response.data.lastReported;
+    })
+
+    .catch((err) => console.error(err));
+
+  if (response.data.recovered = null) {
+    location.innerHTML = 'Data not available';
+  }
+};
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  getData(country.value);
+});
